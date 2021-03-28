d4494078e2facbf155460b367cd58482c8d8e34b
JoeNeu <37147189+JoeNeu@users.noreply.github.com>
2021-03-28 15:14:57 +0200

    init WebService1 - Car Rental Service

---
+++ WebService1/src/main/kotlin/at/serviceengineering/webservice1/webservice1Application.kt
@@ -0,0 +1,11 @@
+package at.serviceengineering.webservice1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebService1Application
+
+fun main(args: Array<String>) {
+	runApplication<WebService1Application>(*args)
+}
