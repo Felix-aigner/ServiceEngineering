04f3719ca10091b4c3b55b6938cad5d3589d35e8
JoeNeu <37147189+JoeNeu@users.noreply.github.com>
2021-03-28 22:06:42 +0200

    remove unused code

--- WebService1/src/main/kotlin/at/serviceengineering/webservice1/dtos/CarReservationUpdateDto.kt
+++ WebService1/src/main/kotlin/at/serviceengineering/webservice1/dtos/CarReservationUpdateDto.kt
@@ -1,8 +1,5 @@
 package at.serviceengineering.webservice1.dtos
 
-import at.serviceengineering.webservice1.enums.Currency
-
 data class CarReservationUpdateDto(
-        var id: String,
-        var currency: Currency
+        var id: String
 )
