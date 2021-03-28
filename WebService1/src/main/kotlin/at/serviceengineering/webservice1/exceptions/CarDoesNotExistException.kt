dfac555a8ccdaff5c4b7e0b803f2a498033cc468
JoeNeu <37147189+JoeNeu@users.noreply.github.com>
2021-03-28 20:47:40 +0200

    rename CarDoesNotExistException.kt

--- WebService1/src/main/kotlin/at/serviceengineering/webservice1/exceptions/CarDoesNotExistException.kt
+++
@@ -1,3 +0,0 @@
-package at.serviceengineering.webservice1.exceptions
-
-class CarDoesNotExistException : Exception("Car does not exist or is already rented")
---
+++ WebService1/src/main/kotlin/at/serviceengineering/webservice1/exceptions/CarNotFoundException.kt
@@ -0,0 +1,3 @@
+package at.serviceengineering.webservice1.exceptions
+
+class CarNotFoundException : Exception("Car does not exist or is already rented")
