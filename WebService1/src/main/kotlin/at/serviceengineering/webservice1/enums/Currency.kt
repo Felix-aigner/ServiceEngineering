e4d08395b28d8eadcc8e38ad81c1aa90ac24a9d1
JoeNeu <37147189+JoeNeu@users.noreply.github.com>
2021-04-02 13:56:40 +0200

    add currency converter fallback

--- WebService1/src/main/kotlin/at/serviceengineering/webservice1/enums/Currency.kt
+++ WebService1/src/main/kotlin/at/serviceengineering/webservice1/enums/Currency.kt
@@ -32,5 +32,6 @@ enum class Currency {
     PHP,
     SGD,
     THB,
-    ZAR
+    ZAR,
+    EUR
 }
--- WebService1/src/main/kotlin/at/serviceengineering/webservice1/services/CurrencyConverterService.kt
+++ WebService1/src/main/kotlin/at/serviceengineering/webservice1/services/CurrencyConverterService.kt
@@ -1,28 +1,36 @@
 package at.serviceengineering.webservice1.services
 
 import at.serviceengineering.webservice1.configuration.CurrencyClient
+import at.serviceengineering.webservice1.configuration.CurrencyRate
+import at.serviceengineering.webservice1.configuration.xPathECB
 import at.serviceengineering.webservice1.enums.Currency
 import at.serviceengineering.webservice1.exceptions.SoapCallException
 import org.springframework.stereotype.Service
 
 @Service
 class CurrencyConverterService(
-        val currencyClient: CurrencyClient
+        val currencyClient: CurrencyClient,
+        val xPathECB: xPathECB
 ) {
 
     fun convertCurrency(value: Float, expectedCurrency: Currency): Float {
-        return if(expectedCurrency == Currency.USD)
-            value
-        else {
-            try {
-                val response = currencyClient.getRate(expectedCurrency)
-                value * response.getRate()
-            } catch(e: Exception) {
-                throw SoapCallException()
-            }
+//        try {
+//            val response = currencyClient.getRate(expectedCurrency)
+//            value * response.getRate()
+//        } catch (e: Exception) {
+//            throw SoapCallException()
+//        }
 
+        // this should be in the catch block as fallback
+        val currencyList = xPathECB.getRates()
+        val dollar = currencyList.filter { currencyRate -> currencyRate.currency == Currency.USD.toString() }.firstOrNull()
+        val expectedRate = currencyList.filter { currencyRate -> currencyRate.currency == expectedCurrency.toString() }.firstOrNull()
+
+        val euro = value / dollar?.rate!!.toFloat()
+        if (expectedCurrency == Currency.EUR) {
+            return euro
         }
+        return euro * expectedRate?.rate!!.toFloat()
     }
-
 
 }
