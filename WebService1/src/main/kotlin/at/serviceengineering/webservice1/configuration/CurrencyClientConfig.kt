2dbaff9c1668a60d7f6c5ad699e566a7a3d48f97
JoeNeu <37147189+JoeNeu@users.noreply.github.com>
2021-04-10 17:26:34 +0200

    add soap client security

--- WebService1/src/main/kotlin/at/serviceengineering/webservice1/configuration/CurrencyClientConfig.kt
+++ WebService1/src/main/kotlin/at/serviceengineering/webservice1/configuration/CurrencyClientConfig.kt
@@ -1,9 +1,12 @@
 package at.serviceengineering.webservice1.configuration
 
+import org.apache.ws.security.WSConstants
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.oxm.jaxb.Jaxb2Marshaller
+import org.springframework.ws.client.support.interceptor.ClientInterceptor
+import org.springframework.ws.soap.security.wss4j2.Wss4jSecurityInterceptor
 
 
 @Configuration
@@ -19,13 +22,31 @@ class CurrencyClientConfig {
         return jaxb2Marshaller
     }
 
+    @Value("\${soap.admin.name}")
+    private val clientToken: String? = null
+
+    @Value("\${soap.admin.secret}")
+    private val clientSecret: String? = null
+
+    @Bean
+    fun securityInterceptor(): Wss4jSecurityInterceptor? {
+        val wss4jSecurityInterceptor = Wss4jSecurityInterceptor()
+        wss4jSecurityInterceptor.setSecurementActions("UsernameToken")
+        wss4jSecurityInterceptor.setSecurementUsername(clientToken)
+        wss4jSecurityInterceptor.setSecurementPassword(clientSecret)
+        wss4jSecurityInterceptor.setSecurementUsernameTokenNonce(true)
+        wss4jSecurityInterceptor.setSecurementPasswordType(WSConstants.PW_DIGEST)
+        return wss4jSecurityInterceptor
+    }
+
     @Bean
     fun currencyClient(jaxb2Marshaller: Jaxb2Marshaller?): CurrencyClient {
         val currencyClient = CurrencyClient()
-        // TODO maybe we have to set the url here
-//        currencyClient.setDefaultUri(webservice2Url)
+        currencyClient.setDefaultUri(webservice2Url)
         currencyClient.setMarshaller(jaxb2Marshaller)
         currencyClient.setUnmarshaller(jaxb2Marshaller)
+        val interceptor: ClientInterceptor? = securityInterceptor()
+        currencyClient.setInterceptors(arrayOf(interceptor))
         return currencyClient
     }
 }
