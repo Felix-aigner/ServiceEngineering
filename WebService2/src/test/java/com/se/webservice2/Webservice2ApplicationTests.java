747701c1865cfa4f486eb5e149e8ad64e185e355
Michael Irimus <michaelirimus@gmail.com>
2021-04-12 18:37:41 +0200

    add Tests for Webservice2, AccountService and CarService

--- WebService2/src/test/java/com/se/webservice2/Webservice2ApplicationTests.java
+++ WebService2/src/test/java/com/se/webservice2/Webservice2ApplicationTests.java
@@ -1,13 +1,38 @@
 package com.se.webservice2;
 
+import localhost._4000.ws.Currency;
+import localhost._4000.ws.GetCurrencyConversionRequest;
+import localhost._4000.ws.GetCurrencyConversionResponse;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
 
-@SpringBootTest
-class Webservice2ApplicationTests {
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class Webservice2ApplicationTests{
+
+	@Autowired TestRestTemplate restTemplate;
 
 	@Test
-	void contextLoads() {
-	}
+	void currencyEndpoint() {
+		GetCurrencyConversionRequest r = new GetCurrencyConversionRequest();
+		r.setBaseCurrency(Currency.USD);
+		r.setBaseValue(new BigDecimal(100));
+		r.setTargetCurrency(Currency.USD);
 
+		HttpEntity<GetCurrencyConversionRequest> request = new HttpEntity<>(r);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+
+		ResponseEntity<GetCurrencyConversionResponse> response = restTemplate.postForEntity("/ws/getCurrencyConversionRequest", request, GetCurrencyConversionResponse.class, headers);
+		assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+	}
 }
