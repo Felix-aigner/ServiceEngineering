73b0d92948dd9f20017e2004016c25d3fa6f5957
Michael Irimus <michaelirimus@gmail.com>
2021-05-10 01:21:35 +0200

    add car microservice integration tests, rental microservice integration tests and setup rest application tests

--- CarMicroservice/src/test/kotlin/at/serviceengineering/microservice/car/CarMicroserviceApplicationTests.kt
+++ CarMicroservice/src/test/kotlin/at/serviceengineering/microservice/car/CarMicroserviceApplicationTests.kt
@@ -1,13 +1,91 @@
 package at.serviceengineering.microservice.car
 
+import at.serviceengineering.microservice.car.entities.Car
+import at.serviceengineering.microservice.car.service.CarService
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import java.math.BigDecimal
 
 @SpringBootTest
 class CarMicroserviceApplicationTests {
 
-	@Test
-	fun contextLoads() {
+	@Autowired
+	val carService: CarService? = null
+
+	val car = Car("testId","testType","testBrand",999, BigDecimal.ONE)
+	val car2 = Car("testId2","testType2","testBrand2",2999, BigDecimal.valueOf(2))
+	val car3 = Car("testId3","testType3","testBrand3",3999, BigDecimal.valueOf(3))
+
+	@BeforeEach
+	fun setup() {
+		cleanUp()
 	}
 
+	@AfterEach
+	fun teardown() {
+		cleanUp()
+	}
+
+	fun cleanUp() {
+		val cars = carService?.findAll()
+
+		if (cars != null) {
+			for (car in cars) {
+				carService?.deleteCar(car.id)
+			}
+		}
+
+		assert(carService!!.isCarRepositoryEmpty())
+	}
+
+	@Test
+	fun addCar() {
+		carService?.addCar(car)
+
+		assert(!carService!!.isCarRepositoryEmpty())
+	}
+
+	@Test
+	fun findOne() {
+		carService?.addCar(car)
+
+		assert(carService?.findOne(car.id) != null)
+	}
+
+	@Test
+	fun findAll() {
+		carService?.addCar(car)
+		carService?.addCar(car2)
+		carService?.addCar(car3)
+
+		val cars = carService?.findAll()
+		assert(cars!!.contains(car))
+		assert(cars.contains(car2))
+		assert(cars.contains(car3))
+	}
+
+	@Test
+	fun isCarRepositoryEmpty() {
+		assert(carService!!.isCarRepositoryEmpty())
+		carService?.addCar(car)
+		assert(!carService!!.isCarRepositoryEmpty())
+		carService?.deleteCar(car.id)
+		assert(carService!!.isCarRepositoryEmpty())
+	}
+
+	@Test
+	fun deleteCar() {
+		carService?.addCar(car)
+
+		var cars = carService?.findAll()
+		assert(cars!!.contains(car))
+
+		carService?.deleteCar(car.id)
+
+		cars = carService?.findAll()
+		assert(!cars!!.contains(car))
+	}
 }
--- RestMicroservice/src/test/kotlin/at/serviceengineering/microservice/rest/service/RestApplicationTests.kt
+++ RestMicroservice/src/test/kotlin/at/serviceengineering/microservice/rest/service/RestApplicationTests.kt
@@ -1,13 +1,47 @@
 package at.serviceengineering.microservice.rest.service
 
+import at.serviceengineering.microservice.rest.service.models.AccountResponse
+import com.google.gson.Gson
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.*
+import org.springframework.http.client.SimpleClientHttpRequestFactory
+import java.net.URI
+import java.util.*
+import javax.annotation.PostConstruct
 
-@SpringBootTest
-class RestApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class RestApplicationTests (@Autowired val restTemplate: TestRestTemplate) {
+	@PostConstruct
+	fun init() {
+		val requestFactory = SimpleClientHttpRequestFactory()
+		requestFactory.setOutputStreaming(false)
+		restTemplate.restTemplate.requestFactory = requestFactory
 
-	@Test
-	fun contextLoads() {
+		createAccountAndSetToken()
 	}
 
+	val account = "{'id':'123','username':'123','password':'123','firstname':'firstname','lastname':'lastname','isAdministrator':false}"
+	var token = ""
+
+	fun createAccountAndSetToken () {
+		restTemplate.postForEntity(URI("/accounts"), account, String::class.java)
+		val res = Gson().fromJson(restTemplate.postForEntity(URI("/accounts/login"), account, String::class.java).body, AccountResponse::class.java)
+		token = res.token
+	}
+
+	@Test
+	fun getAllAccounts() {
+		val headers = HttpHeaders()
+		headers.accept = Collections.singletonList(MediaType.APPLICATION_JSON)
+		headers.set("token", token)
+
+		val entity = HttpEntity(null, headers)
+
+		val res = restTemplate.exchange(URI("/accounts"), HttpMethod.GET, entity, String::class.java)
+		Assertions.assertThat(res.statusCode).isEqualTo(HttpStatus.OK)
+	}
 }
